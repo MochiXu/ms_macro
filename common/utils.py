18c1d2ef84b972abbc8b6ec564509ffb0da35b08
mochi <mochi_xu@icloud.com>
2024-09-02 17:26:55 +0800

    fix

--- common/utils.py
+++ common/utils.py
@@ -32,9 +32,7 @@ def compute_sparse_vector(texts: List[str], model, tokenizer=AutoTokenizer.from_
     Returns:
     torch.Tensor: Computed vector.
     """
-    print(model.device)
     tokens = tokenizer(texts, padding=True, return_tensors="pt").to(model.device)
-
     output = model(**tokens)
     logits, attention_mask = output.logits, tokens.attention_mask
     relu_log = torch.log(1 + torch.relu(logits))
